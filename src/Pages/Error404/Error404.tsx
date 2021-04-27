40a015c6fe0bc6d54c324b5acb9405864e12481d
Jesus_Lares <jesus.lares2000@gmail.com>
2021-04-27 15:59:24 -0500

    Se creo la interfaz de la pagina error404

--- src/Pages/Error404/Error404.tsx
+++ src/Pages/Error404/Error404.tsx
@@ -1,8 +1,19 @@
+import { Link } from 'react-router-dom'
+
+import "./Error404.scss"
 
 const Error404 = () => {
     return (
-        <div>
-            Error404
+        <div className="error" >
+            <div className="error_content">
+                <h1>404</h1>
+                <h4>Pagina no encontrada</h4>
+                <p>Lo siento, la pagina que estas buscando no existe. Si piensas que algo esta roto, reporta el problema </p>
+                <div className="error_content-btns">
+                    <Link to="/" >Ir a Home </Link>
+                    <a href="mailto:contacto@jesuslares.com?subject=Problemas sobre la pagina Movie Recommendation">Reportar problema </a>
+                </div>
+            </div>
         </div>
     )
 }
